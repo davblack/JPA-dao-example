fc00529987014f6b711a86c0155799853ccda787
davblack <davidfekete51@gmail.com>
2020-05-02 13:04:15 +0200

    done

---
+++ src/main/java/book/BookDao.java
@@ -0,0 +1,20 @@
+package book;
+
+import book.model.Book;
+import jpa.*;
+import javax.persistence.*;
+import javax.transaction.*;
+import java.util.*;
+
+public class BookDao extends GenericJpaDao <Book> {
+    @Transactional
+    public Optional <Book> findByIsbn13 (String isbn13) {
+        try { return Optional.of(entityManager.createQuery("select x from Book x where x.isbn13 =: isbn13", Book.class)
+                    .setParameter("isbn13", isbn13)
+                    .getSingleResult());
+        }
+        catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+}
